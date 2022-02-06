8e6e500338e1ee163d6e66048731ddb23ea0b72e
Raffaele Morí <mori.raffaele@gmail.com>
2022-02-06 20:28:42 +0000

    Base created

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { RightSideNumberInput } from 'react-right-side-number-input'
+import 'react-right-side-number-input/dist/index.css'
+
+const App = () => {
+  return <RightSideNumberInput text="Create React Library Example 😄" />
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+export const RightSideNumberInput = ({ text }) => {
+  return <div className={styles.test}>Example Component: {text}</div>
+}
