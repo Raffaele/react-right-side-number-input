5a5d2f15662f3e5f2003fb43281a481543c4b80b
Raffaele <mori.raffaele@gmail.com>
2022-03-19 16:43:48 +0000

    Updates

---
+++ precache-manifest.a7f7994b4fe01bab38fd60f4d90d5129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a279a8e0092625a2100d58db9d46b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ae0317eae3224a715e9",
+    "url": "./static/css/main.07b70bef.chunk.css"
+  },
+  {
+    "revision": "6fa3b3ea6be9f67106b3",
+    "url": "./static/js/2.e9b49a77.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e9b49a77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae0317eae3224a715e9",
+    "url": "./static/js/main.a1320d0c.chunk.js"
+  },
+  {
+    "revision": "1e0c8caf8f8bcff9b565",
+    "url": "./static/js/runtime-main.5fe3c1f6.js"
+  }
+]);
